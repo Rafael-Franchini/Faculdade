1428ae8a6f5d77092408d2fdb3122b65ecac4316
Rafael <80691464+Rafael-Franchini@users.noreply.github.com>
2022-09-17 20:25:43 -0300

    ok

--- programas/browser/browser.py
+++ programas/browser/browser.py
@@ -1,1 +1,14 @@
-import 
+from PyQt5.QtWidgets import*
+import sys
+from PyQt5.QtWebEngineWidgets import*
+from PyQt5.QtCore import*
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser= QWebEngineView()
+        self.showMaximized()
+App = QApplication(sys.argv)
+QApplication.setApplicationName('Meu_browser')
+windows=MainWindow()
+App.exec()
