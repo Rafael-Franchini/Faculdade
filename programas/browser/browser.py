75e6534cfa43d5053e6f6681d49d354582aedeed
Rafael <80691464+Rafael-Franchini@users.noreply.github.com>
2022-09-17 21:08:00 -0300

    browser

--- programas/browser/browser.py
+++ programas/browser/browser.py
@@ -1,3 +1,4 @@
+"""Importando bibliotecas"""
 from PyQt5.QtWidgets import*
 import sys
 from PyQt5.QtWebEngineWidgets import*
@@ -7,8 +8,12 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.browser= QWebEngineView()
+        self.browser.setUrl(QUrl('http://www.google.com'))
+        self.setCentralWidget(self.browser)
         self.showMaximized()
 App = QApplication(sys.argv)
 QApplication.setApplicationName('Meu_browser')
 windows=MainWindow()
+
+"""Executando app"""
 App.exec()
