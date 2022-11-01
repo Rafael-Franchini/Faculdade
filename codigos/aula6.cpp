a3de4ecf5e0daed4a8922452f385b3b3af8ed85d
Rafael <80691464+Rafael-Franchini@users.noreply.github.com>
2022-11-01 20:21:46 -0300

    aula fila por ponteiro

--- codigos/aula6.cpp
+++ codigos/aula6.cpp
@@ -1,31 +1,55 @@
+
 #include <iostream>
 using namespace std;
 
+//estrutura
 struct no{
   int valor;
   no * proximo;
 };
 
+//retorna primeiro valor
+int first(){
+  return inicio -> valor;
+}
+//retorna ultimo valor
+int back(){
+  return fim -> valor;
+}
+//ponteiros inicio e fim
 no * inicio= NULL;
-
 no * fim = NULL;
 
+//colocar/enfileirar 
 void enqueue(int novoValor){
   no * novoNo =(no*)malloc(sizeof(no));
   novoNo -> valor = novoValor;
   novoNo -> proximo = NULL;
+  //se inicio estiver vazio criar valor apenas...
   if(inicio==NULL){
     inicio = novoNo;
     fim = novoNo;
   }
+  //senao fazer final do primeiro elemento apontar o proximo ...
   else{
     fim -> proximo = novoNo;
     fim = novoNo;
   }
 }
 
-int first(){
-  return inicio->valor;
+//retirar valor
+void dequeue(){
+  no * removido = inicio;
+  //se o inicio for igual ao fim ele zera tudo... 
+  if(inicio==fim){
+    inicio = NULL;
+    fim = NULL;
+  }
+  //senao colocar o proximo valor como inicio...
+  else{
+    inicio=inicio->proximo;
+  }
+  free(removido);
 }
 
 int main(){
